acdeefe15622f214ceb4386ba6b6c100d9e4b80f
Jianqi Jin <arian.jianqi@gmail.com>
2023-11-05 18:56:09 -0600

    feat: update rank

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import styles from './index.module.scss';
 import GlobalState from './store/global';
 import { useContainer } from 'unstated-next';
+
 function App() {
   const { count, setCount } = useContainer(GlobalState);
   return (
     <>
-      <div className={styles.App}>
+      <div className={`${styles.App} h-[100vh]`}>
         Demo
-        <div className="flex justify-center">
+        <div>
           <div>{count}</div>
           <div>
             <button onClick={() => setCount((val) => val + 1)}>+1</button>
